289b26393b530e006809d230209391a10ad3cc14
Ahmed Atiyah <ahmedmaheratiyah@gmail.com>
2019-09-23 14:09:40 +0200

    small test with bigger pic

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
         className={'cover'}
         alt={'woman'}
         overlaySrc={
-          'https://images.pexels.com/photos/2177009/pexels-photo-2177009.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=5'
+          'https://images.pexels.com/photos/2156416/pexels-photo-2156416.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=5'
         }
         src={
-          'https://images.pexels.com/photos/2177009/pexels-photo-2177009.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+          'https://images.pexels.com/photos/2156416/pexels-photo-2156416.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
         }
       />
     </div>
